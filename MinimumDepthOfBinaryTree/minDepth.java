0b005266c40a8b102f58cc84850b5c75730cecd0
Danying Yang <15757124551@163.com>
2017-07-21 13:55:46 +0800

    MinimumDepthOfBinaryTree

--- MinimumDepthOfBinaryTree/minDepth.java
+++ MinimumDepthOfBinaryTree/minDepth.java
@@ -10,6 +10,8 @@
 public class Solution {
     public int minDepth(TreeNode root) {
         if(root == null) return 0;
-        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        return (left == 0 || right == 0) ? left + right + 1: Math.min(left, right) + 1;
     }
 }

7ca2e9aa8e68e64eb2cae91851f137dfd97bb395
Danying Yang <15757124551@163.com>
2017-07-22 08:59:33 +0800

    isBalanced

---
+++ BalancedBinaryTree/isBalanced.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return getDepth(root) != -1;
+    }
+    public int getDepth(TreeNode root) {
+        if(root == null) return 0;
+        int left = getDepth(root.left);
+        if(left != -1) {
+            int right = getDepth(root.right);
+            if(right != -1) {
+                return Math.abs(left - right) <= 1 ? Math.max(left, right) + 1 : -1; 
+            }
+        }
+        return -1;
+    }
+}

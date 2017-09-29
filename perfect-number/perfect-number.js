6b6a506c77edbbab8d4eff08d54a6f94770eecc0
Danying Yang <15757124551@163.com>
2017-09-29 09:12:26 +0800

    perfect-number

---
+++ perfect-number/perfect-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var checkPerfectNumber = function(num) {
+    if (num == 1) return false;
+    let sum = 1;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i == 0) 
+            sum = sum + i + num/i ;
+    }
+    return sum == num;
+};

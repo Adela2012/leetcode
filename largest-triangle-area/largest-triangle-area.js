5dff6dd2f42b62c3327b0de7c011b78b9bac0062
Danying Yang <15757124551@163.com>
2018-08-07 15:51:01 +0800

    Largest Triangle Area

---
+++ largest-triangle-area/largest-triangle-area.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var largestTriangleArea = function (points) {
+  let res = 0
+  for (let p1 of points) {
+    for (let p2 of points) {
+      for (let p3 of points) {
+        res = Math.max(res, helper(p1, p2, p3))
+      }
+    }
+  }
+  return res
+};
+
+var helper = function (p1, p2, p3) {
+  return 0.5 * Math.abs(p1[0] * p2[1] + p2[0] * p3[1] + p3[0] * p1[1] - p1[0] * p3[1] - p3[0] * p2[1] - p2[0] * p1[1])
+}

4f57ab8ae8e870faa1c9073da9823d3eda611e44
Danying Yang <15757124551@163.com>
2018-09-18 15:57:33 +0800

    Binary Search

---
+++ binary-search/binary-search.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+  let left = 0
+  let right = nums.length - 1
+  while (left < right) {
+      let mid = left + right >> 1
+      if(nums[mid] == target) return mid
+      if(nums[mid] > target) right = mid
+      else left = mid + 1
+  }
+  return nums[left] == target ? left : -1
+};

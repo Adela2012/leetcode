b9b958645611a8d20e616d6539c20fc65552a365
Danying Yang <15757124551@163.com>
2018-10-04 16:41:50 +0800

    Best Time to Buy and Sell Stock

---
+++ BestTimeToBuyAndSellStock/maxProfit3.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let minPrice = Number.MAX_VALUE 
+    let maxProfit = 0
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i]
+        } else if (maxProfit < prices[i] - minPrice) {
+            maxProfit = prices[i] - minPrice
+        }
+    }
+    return maxProfit
+};

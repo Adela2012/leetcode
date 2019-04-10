a931f252f6a24a930413eda774bb218df8bdac4f
Adela2012 <15757124551@163.com>
2019-04-10 17:52:19 +0800

    509. Fibonacci Number

---
+++ 509. Fibonacci Number/index.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} N
+ * @return {number}
+ */
+var fib = function(N) {
+  let arr = [0, 1]
+  for (let i = 2; i <=N; i++) {
+      arr[i] = arr[i-1] + arr[i-2] 
+  }
+  return arr[N]
+};

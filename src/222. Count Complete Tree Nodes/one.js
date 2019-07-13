6ee9580d5a783bdc649207ef8a44e76a864b9125
Adela2012 <15757124551@163.com>
2019-07-13 23:23:38 +0800

    222. Count Complete Tree Nodes

---
+++ src/222. Count Complete Tree Nodes/one.js
@@ -0,0 +1,15 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var countNodes = function(root) {
+  if(!root) return 0;
+  return 1 + countNodes(root.left) + countNodes(root.right);
+};

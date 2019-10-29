df9b3d0c0686a473c0b0f36faea451ad41c9f761
Adela2012 <15757124551@163.com>
2019-10-29 10:54:44 +0800

    55. Jump Game

---
+++ src/55. Jump Game/one.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    let lastpos = nums.length - 1
+    for (let i = nums.length - 2; i>=0; i--) {
+      if (i + nums[i] >= lastpos) 
+        lastpos = i
+    }
+    return lastpos == 0
+};

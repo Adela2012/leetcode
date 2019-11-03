f0ef099f652c7c5d26aed835b8a0aec6e10e138e
Adela2012 <15757124551@163.com>
2019-11-03 13:23:31 +0800

    73. Set Matrix Zeroes

--- src/73. Set Matrix Zeroes/two.js
+++ src/73. Set Matrix Zeroes/two.js
@@ -2,26 +2,39 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-var setZeroes = function(matrix) {
+var setZeroes = function (matrix) {
   let isCol = false
-  let R = matrix.length, C = matrix[0].length
+  let R = matrix.length
+  let C = matrix[0].length
 
-  for (let r = 0; r < R; r++) {
-    if (matrix[r][0] == 0) isCol = true
+  for (let i = 0; i < R; i++) {
+    if (matrix[i][0] == 0) isCol = true
 
-    for (let c = 1; c < C; c++) {
-      if (matrix[r][c] == 0) {
-        matrix[0][c] = 0
-        matrix[r][0] = 0
+    for (let j = 1; j < C; j++) {
+      if (matrix[i][j] == 0) {
+        matrix[0][j] = 0
+        matrix[i][0] = 0
       }
     }
   }
 
-  for (let r = 1; r < R; r++) {
-    for (let c = 1; c < C; c++) {
-      if (matrix[r][0] == 0 || matrix[0][c] == 0) {
-        matrix[r][c] = 0
+  for (let i = 1; i < R; i++) {
+    for (let j = 1; j < C; j++) {
+      if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+        matrix[i][j] = 0
       }
+    }
+  }
+
+  if (matrix[0][0] == 0) {
+    for (let j = 0; j < C; j++) {
+      matrix[0][j] = 0
+    }
+  }
+
+  if (isCol) {
+    for (let i = 0; i < R; i++) {
+      matrix[i][0] = 0
     }
   }
 
